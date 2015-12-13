d75f68b2bb90c61068aa4e9f2fe3b306ddf029af
andystanton <andystanton@gmail.com>
2015-12-13 22:22:18 +0000

    switch to sw-toolbox

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,13 @@
-var cacheVersion = "v1"
+importScripts('bower_components/sw-toolbox/sw-toolbox.js');
 
-self.addEventListener('install', event => event.waitUntil(
-  caches.open(cacheVersion).then(cache =>
-    cache.addAll([
-      '/',
-      '/sw.js',
-      '/posts',
-      '/bower_components/angular/angular.js',
-      '/bower_components/angular/angular.min.js',
-      '/bower_components/bootstrap/dist/css/bootstrap.min.css',
-      '/bower_components/bootstrap/dist/js/bootstrap.min.js',
-      '/bower_components/jquery/dist/jquery.min.js'
-    ]).then(() =>
-      self.skipWaiting()
-    )
-  )
-));
+toolbox.precache([
+  '/',
+  '/sw.js',
+  '/posts',
+  '/bower_components/angular/angular.min.js',
+  '/bower_components/bootstrap/dist/css/bootstrap.min.css',
+  '/bower_components/bootstrap/dist/js/bootstrap.min.js',
+  '/bower_components/jquery/dist/jquery.min.js'
+]);
 
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-
-self.addEventListener('fetch', event => event.respondWith(
-  Promise.all([
-    caches.open(cacheVersion),
-    fetch(event.request)
-  ]).then(values => {
-    var cache = values[0];
-    var response = values[1].clone();
-    console.log(`caching ${event.request.url}`)
-    cache.put(event.request, values[1])
-    return response
-  }).catch(error => {
-    console.log(`fetch error: ${error}`)
-    return caches.open(cacheVersion).then(cache => cache.match(event.request))
-  })
-));
+toolbox.router.default = toolbox.networkFirst;
