68a93ddf4c7d00c4297fefd890990b6d7d4bd1c6
andystanton <andystanton@gmail.com>
2015-12-13 11:17:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Prototype Progressive App
+
+A prototype application to experiment building a progressive app.
+
+## Requirements
+
+ * Node JS 5.2.0
+ * A browser that [supports Service Worker](http://caniuse.com/#feat=serviceworkers)
+
+## Usage
+
+```sh
+$ npm install
+$ npm start
+```
+
+The application is now serving at `http://localhost:3000/`.
+
+## Features
+
+ * Offline mode via Service Worker
---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var Datastore = require('nedb'),
+  db = new Datastore({
+    filename: 'data/posts.db',
+    autoload: true
+  });
+
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.locals.pretty = true;
+
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Prototype Progressive App'
+  }));
+
+app.get('/posts', (req, res) =>
+  new Promise((resolve, reject) =>
+    db.find({
+      state: 'published'
+    }, (error, docs) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(docs)
+    })).then(docs => res.json(docs)));
+
+app.get('/drafts', (req, res) =>
+  new Promise((resolve, reject) =>
+    db.find({
+      state: 'draft'
+    }, (error, docs) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(docs)
+    })).then(docs => res.json(docs)));
+
+app.put('/post', (req, res) => {
+  console.log("writing post with body")
+  console.log(req.body);
+
+  return new Promise((resolve, reject) => {
+    if (req.body._id) {
+      console.log(`updating ${req.body._id}`)
+      db.update({
+        _id: req.body._id
+      }, req.body, {}, (err, numReplaced) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(req.body);
+        }
+      });
+    } else {
+      console.log("creating")
+      db.insert(req.body, (err, numReplaced) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(req.body);
+        }
+      });
+    }
+  }).then(() => res.sendStatus(204))
+});
+
+app.delete('/post/:id', (req, res) => {
+  db.remove({
+    '_id': req.params.id
+  })
+  res.sendStatus(204)
+});
+
+var server = app.listen(3000, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
